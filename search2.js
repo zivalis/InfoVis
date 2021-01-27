4f7d0e2bd631715d11c5b451041b24c9676788c4
baloghsz <74829126+baloghsz@users.noreply.github.com>
2021-01-27 03:00:38 +0100

    Searchbar umgebaut.
    Durch Auwahl des Bundeslandes wird das Bundesland ausgewählt. Das beeinflusst die Dropdown de Regionwahl nicht.

--- search2.js
+++ search2.js
@@ -1,16 +1,24 @@
 function myFunction(){
-    var input, filter, ul, li, a, i, txtValue;
-    input= document.getElementById("myInput");
+    var input, filter, ul, li, a, i, txtValue, bund;
+    bund= document.getElementById("selectBund").value;
     filter= input.value.toUpperCase();
     ul = document.getElementById("Regi");
     li= ul.getElementsByTagName("li");
+
+    
     for(i = 0; i<li.length; i++){
         a = li[i].getElementsByTagName("a")[0];
+        input= li[i].value;
         txtValue = a.textContent || a.innerText;
-        if(txtValue.toUpperCase().indexOf(filter) > -1){
+        if(txtValue.toUpperCase().indexOf(filter) > -1 && bund==input){
             li[i].style.display = "";
         }else{
             li[i].style.display = "none";
         }
     }
+}
+
+function change(){
+    var x = document.getElementById("selectBund").value;
+    RegionChange(x);
 }
