f1116d49487c49d5fd21ff3369ca1574e7847259
baloghsz <74829126+baloghsz@users.noreply.github.com>
2021-01-27 13:25:28 +0100

    Bug gfixt.

--- search2.js
+++ search2.js
@@ -1,16 +1,13 @@
 function myFunction(){
-    var input, filter, ul, li, a, i, txtValue, bund;
-    bund= document.getElementById("selectBund").value;
+    var input, filter, ul, li, a, i, txtValue;
+    input= document.getElementById("myInput");
     filter= input.value.toUpperCase();
     ul = document.getElementById("Regi");
     li= ul.getElementsByTagName("li");
-
-    
     for(i = 0; i<li.length; i++){
         a = li[i].getElementsByTagName("a")[0];
-        input= li[i].value;
         txtValue = a.textContent || a.innerText;
-        if(txtValue.toUpperCase().indexOf(filter) > -1 && bund==input){
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
             li[i].style.display = "";
         }else{
             li[i].style.display = "none";
